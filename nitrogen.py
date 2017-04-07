03252aa62135ff926377f2a344436487ab3b7b9e
Randy Gingeleski <rjg26247@gmail.com>
2017-04-07 08:32:36 -0400

    login functionality up to credentials submit

--- nitrogen.py
+++ nitrogen.py
@@ -1,5 +1,7 @@
+from bs4 import BeautifulSoup
 import os
 from selenium import webdriver
+from time import sleep
 
 class NitrogenApi():
     """
@@ -11,18 +13,42 @@ class NitrogenApi():
         Constructor, launch the web driver
         """
 
+        self.browser = None
+        # Get this person's Nitrogen login credentials from env vars
+        self.login_user = os.getenv('NitrogenUser')
+        if self.login_user is None:
+            print('You must set environment variable "NitrogenUser" to your username')
+            exit()
+        self.login_pass = os.getenv('NitrogenPass')
+        if self.login_pass is None:
+            print('You must set environment variable "NitrogenPass" to your password')
+            exit()
+        # Set up the browser driver
         browser_exe = '.' + os.sep + 'chromedriver' + os.sep + 'chromedriver.exe'
         self.browser = webdriver.Chrome(browser_exe)
-        self.browser.implicitly_wait(15)
+        self.browser.implicitly_wait(5)
 
-    def __del__(self):
+    def login(self):
+        """
+        Log in to Nitrogen Sports
+        """
+        self.browser.get('https://nitrogensports.eu')
+        sleep(15)
+        self.browser.find_element_by_css_selector('div#modal-welcome-login-button').click()
+        sleep(5)
+        self.browser.find_element_by_css_selector('#modal-account-login-username-textbox').send_keys(self.login_user)
+        self.browser.find_element_by_css_selector('#modal-account-login-password-textbox').send_keys(self.login_pass)
+        self.browser.find_element_by_css_selector('#modal-account-login-button').click()
+
+'''    def __del__(self):
         """
         Close the browser when we delete the object
         """
-        
-        self.browser.close()
+
+        if self.browser is not None:
+            self.browser.close()'''
 
 # The following is debug/test while developing this API...
 if __name__ == '__main__':
     NITRO_API = NitrogenApi()
-    NITRO_API.browser.get('https://github.com')
+    NITRO_API.login()
