ddb232097fa0acca7beb258dd4da5995c1812694
Randy Gingeleski <rjg26247@gmail.com>
2017-04-14 07:52:20 -0400

    working on alternate unofficial api approach

--- nitrogen2.py
+++ nitrogen2.py
@@ -14,30 +14,59 @@ class NitrogenApi():
         Constructor
         """
 
+        self.session = None
         self.polling_sid = None
         self.ping_count = 0
+
+    def session_check(self):
+        """
+        Check for session existence
+        """
+        if self.session is None:
+            self.session = requests.Session()
+
+    def do_cloudflare(self):
+        """
+        Complete the Cloudflare check for this session
+        
+        TODO
+        """
+        pass
 
     def login(self):
         """
         Login
         """
+        self.session_check()
         #username = 'YOUR_USERNAME'
         username = 'flot989'
         #password = 'YOUR_PASSWORD'
         password = 'Thr0wAway1'
         login_url = BASE_URL + 'php/login/login.php'
         payload = {'username': username, 'password': password, 'otp': '', 'captcha_code': ''}
-        requests.post(login_url, data=payload, verify=False)
+        self.session.post(login_url, data=payload, verify=False)
 
     def ping(self):
         """
-        TODO TODO
         Send the server heartbeat / keep-alive / ping
+
+        TODO fix
         """
         unix_time = int(time.time())
         ping_url = BASE_URL + PING_URL_START + '&t=' + str(unix_time) + '-' + str(self.ping_count)
+        self.session.post(ping_url, verify=False)
         self.ping_count += 1
-        requests.post(ping_url)
+
+    def find_upcoming_games(self, sport='Soccer'):
+        """
+        Request upcoming games for the given sport
+        """
+        games_url = BASE_URL + 'php/query/findgames_upcomming.php'
+        payload = {'sport': sport}
+        req = self.session.post(games_url, data=payload, verify=False)
+        if req.status_code == requests.codes.ok:
+            return req.json()
+        return None
 
 # The following is debug/test while developing this API...
 if __name__ == '__main__':
