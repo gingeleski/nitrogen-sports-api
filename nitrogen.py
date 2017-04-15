72132a5c5ce0c5cc22b40715fb9231861beb9b03
Randy Gingeleski <rjg26247@gmail.com>
2017-04-14 21:07:14 -0400

    make 'alternate api approach' the main one

--- nitrogen.py
+++ nitrogen.py
@@ -1,54 +1,114 @@
-from bs4 import BeautifulSoup
-import os
-from selenium import webdriver
-from time import sleep
+import cfscrape
+import json
+import requests
+import time
+
+# Suppress HTTPS warnings
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+BASE_URL = 'https://nitrogensports.eu/'
+PING_URL_START = 'socket.io/?EIO=3&transport=polling'
 
 class NitrogenApi():
     """
     Interface to programmatically interact with Nitrogen Sports
     """
 
-    def __init__(self):
+    def __init__(self, auto_start_session=True):
         """
-        Constructor, launch the web driver
+        Constructor
         """
 
-        self.browser = None
-        # Get this person's Nitrogen login credentials from env vars
-        self.login_user = os.getenv('NitrogenUser')
-        if self.login_user is None:
-            print('You must set environment variable "NitrogenUser" to your username')
-            exit()
-        self.login_pass = os.getenv('NitrogenPass')
-        if self.login_pass is None:
-            print('You must set environment variable "NitrogenPass" to your password')
-            exit()
-        # Set up the browser driver
-        browser_exe = '.' + os.sep + 'chromedriver' + os.sep + 'chromedriver.exe'
-        self.browser = webdriver.Chrome(browser_exe)
-        self.browser.implicitly_wait(5)
+        self.session = None
+        self.polling_sid = None
+        self.ping_interval = -1
+        self.ping_count = 0
+        if auto_start_session is True:
+            self.new_session()
+
+    def new_session(self):
+        """
+        Launch a new session
+        """
+        self.session = cfscrape.CloudflareScraper()
+        self.pass_cloudflare()
+
+    def pass_cloudflare(self):
+        """
+        Complete the Cloudflare check for this session
+        """
+        self.session.get(BASE_URL, verify=False)
 
     def login(self):
         """
-        Log in to Nitrogen Sports
+        Login
         """
-        self.browser.get('https://nitrogensports.eu')
-        sleep(15)
-        self.browser.find_element_by_css_selector('div#modal-welcome-login-button').click()
-        sleep(5)
-        self.browser.find_element_by_css_selector('#modal-account-login-username-textbox').send_keys(self.login_user)
-        self.browser.find_element_by_css_selector('#modal-account-login-password-textbox').send_keys(self.login_pass)
-        self.browser.find_element_by_css_selector('#modal-account-login-button').click()
+        username = 'flot989'
+        password = 'Thr0wAway1'
+        login_url = BASE_URL + 'php/login/login.php'
+        payload = {'username': username, 'password': password, 'otp': '', 'captcha_code': ''}
+        self.session.post(login_url, data=payload, verify=False)
 
-'''    def __del__(self):
+    def logout(self):
         """
-        Close the browser when we delete the object
+        Logout
         """
+        logout_url = BASE_URL + 'php/login/logout.php'
+        self.session.post(logout_url, verify=False)
 
-        if self.browser is not None:
-            self.browser.close()'''
+    def ping(self):
+        """
+        Send the server heartbeat / keep-alive / ping
+        """
+        unix_time = int(time.time())
+        ping_url = BASE_URL + PING_URL_START + '&t=' + str(unix_time)
+        ping_url = ping_url + '-' + str(self.ping_count)
+        req = None
+        if self.ping_count == 0:
+            req = self.session.get(ping_url, verify=False)
+            poll_info_json = json.loads(req.text[req.text.find('{'):])
+            self.polling_sid = poll_info_json['sid']
+            self.ping_interval = poll_info_json['pingInterval'] / 1000
+        else:
+            ping_url = ping_url + '&sid=' + self.polling_sid
+            req = self.session.post(ping_url, verify=False)
+        self.ping_count += 1
+
+    def keep_alive(self, duration=None):
+        """
+        Keep the session alive by pinging at a normal interval
+
+        Args:
+            duration: how long to keep alive, in seconds
+        """
+        now = int(time.time())
+        if duration is not None:
+            end_time = now + duration
+        else:
+            end_time = None
+        next_time = now
+        looping = True
+        while looping is True:
+            if duration is not None and now > end_time:
+                looping = False
+            elif now >= next_time:
+                self.ping()
+                next_time = now + self.ping_interval
+            time.sleep(1)
+            now = int(time.time())
+
+    def find_upcoming_games(self, sport='Soccer'):
+        """
+        Request upcoming games for the given sport
+        """
+        games_url = BASE_URL + 'php/query/findgames_upcomming.php'
+        payload = {'sport': sport}
+        req = self.session.post(games_url, data=payload, verify=False)
+        if req.status_code == requests.codes.ok:
+            return req.json()
+        return None
 
 # The following is debug/test while developing this API...
 if __name__ == '__main__':
     NITRO_API = NitrogenApi()
-    NITRO_API.login()
