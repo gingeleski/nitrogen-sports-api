b907fd040c7a92d8ec990a1219a69f37c944d3cf
Randy Gingeleski <rjg26247@gmail.com>
2017-04-16 20:03:24 -0400

    add basic game inspection to test betting system

--- soccer_draws_system.py
+++ soccer_draws_system.py
@@ -1,10 +1,27 @@
+import json
 from nitrogen import NitrogenApi
 import time
 
-NITRO_API = NitrogenApi()
-NITRO_API.login('flot989', 'Thr0wAway1')
-time.sleep(1)
-games = NITRO_API.find_upcoming_games()
-print(games)
-time.sleep(1)
-NITRO_API.logout()
+USE_TEST_DATA = True
+
+games_json = None
+
+if USE_TEST_DATA is True:
+    with open('test_upcoming_games_json.txt') as data_file:    
+        games_json = json.load(data_file)
+else:
+    NITRO_API = NitrogenApi()
+    NITRO_API.login('flot989', 'Thr0wAway1')
+    time.sleep(1)
+    games_json = NITRO_API.find_upcoming_games()
+    time.sleep(1)
+    NITRO_API.logout()
+
+for event in games_json['data']:
+    print('EVENT: ' + event['event_id'])
+    for period in event['period']:
+        print('\t' + 'PERIOD: ' + period['period_id'])
+        if 'moneyLine' in period and period['moneyLine'] is not None:
+            for line in period['moneyLine']:
+                if 'drawPrice' in line:
+                    print('\t\t' + 'Found avail draw bet @ ' + line['drawPrice'])
