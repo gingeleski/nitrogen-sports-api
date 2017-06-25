c3f7a22a67f28647072e5ae921e8db140c497ab0
Randy Gingeleski <rjg26247@gmail.com>
2017-06-25 19:09:54 -0400

    begin cleaning and reorganizing code

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='nitrogen-sports-api',
+    version='0.1',
+    description='Unofficial Nitrogen Sports API.',
+    url='https://github.com/gingeleski/nitrogen-sports-api',
+    author='Randy Gingeleski',
+    author_email='rjg26247@gmail.com',
+    license='GNU',
+    packages=['NitrogenAPI'],
+    zip_safe=False,
+    install_requires=[
+        'appdirs==1.4.3',
+        'cfscrape==1.8.0',
+        'packaging==16.8',
+        'PyExecJS==1.4.0',
+        'pyparsing==2.2.0',
+        'requests==2.13.0',
+        'six==1.10.0'
+    ]
+)
